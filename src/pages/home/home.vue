351bd6a3c6f31f3401826f19d474f7d212871daf
cao ziLiang <m18500384190@qq.com>
2019-06-01 17:21:25 +0800

    enter

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="home">home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="">
+.home {
+  font-size: 20px;
+  color: red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/pages/home/home'
+import list from '@/pages/list/list'
 
 Vue.use(Router)
 
@@ -8,8 +9,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: home
+    }, {
+      path: '/list',
+      component: list
     }
   ]
 })
