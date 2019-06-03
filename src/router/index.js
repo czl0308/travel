986c775262d4602c14e93e4df9c7fafc37071fa2
cao ziLiang <m18500384190@qq.com>
2019-06-03 11:17:46 +0800

    修改index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/pages/home/home'
+import Home from '@/pages/home/home'
 import list from '@/pages/list/list'
 
 Vue.use(Router)
@@ -9,7 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: home
+      component: Home
     }, {
       path: '/list',
       component: list
