46754725fb6ed6cd18302d04ec6c7c5734bd3f04
cao ziLiang <m18500384190@qq.com>
2019-06-03 11:20:58 +0800

    修改list

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/home'
-import list from '@/pages/list/list'
+import List from '@/pages/list/list'
 
 Vue.use(Router)
 
@@ -11,8 +11,8 @@ export default new Router({
       path: '/',
       component: Home
     }, {
-      path: '/list',
-      component: list
+      path: '/List',
+      component: List
     }
   ]
 })
