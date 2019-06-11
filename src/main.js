fca2fe80c3334393a610dfe17a915370e701602f
cao ziLiang <m18500384190@qq.com>
2019-06-11 09:17:43 +0800

    添加字体图标库

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// webpack的入口文件
+
+// 引入vue,根组件,路由
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入移动端点击库,防止 300 延迟和点击穿透
 import fastclick from 'fastclick'
 // 重置默认样式
-import './assets/css/reset.css'
-// 1 像素边框
-import './assets/css/border.css'
+import './assets/styles/reset.css'
+// 1像素边框
+import './assets/styles/border.css'
 
 // 阻止 vue 在启动时生成生产提示。
 Vue.config.productionTip = false
--- src/pages/components/homeHeader.vue
+++ src/pages/components/homeHeader.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="header">
-    <div class="header-left">hello world</div>
-    <div class="header-input"></div>
-    <div class="header-right"></div>
+    <div class="header-left">
+      <!-- <span class="iconfont iconzuo"></span> -->
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#iconzuo"></use>
+      </svg>
+    </div>
+    <div class="header-input">输入城市景点</div>
+    <div class="header-right">城市</div>
   </div>
 </template>
 
@@ -12,5 +17,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    height: 43px
+    background: #00bcd4
+    line-height: 43px
+    color: #fff
+    .header-left
+      width: 32px
+      font-size: 32px
+    .header-input
+      flex: 1
+      background: #fff
+      margin: 8px 8px
+      padding-left: 10px
+      line-height: 27px
+      border-radius: 3px
+      color: #999
+    .header-right
+      width: 62px
+      text-align: center
 </style>
